b83f6ff7a5c7b740c4681a8a41ff2d8465c0e2a8
benwu <wubinben@icloud.com>
2019-07-03 16:32:30 +0800

    refactor

--- src/main/java/kata/taxi/Taximeter.java
+++ src/main/java/kata/taxi/Taximeter.java
@@ -4,14 +4,18 @@ public class Taximeter {
 
     public static final int INITIAL_CHARGE_DISTANCE = 2;
     public static final int INITIAL_CHARGE = 6;
+    public static final int SURCHARGE_DISTANCE = 8;
+    public static final double CHARGE_PER_KILOMETER = 0.8;
+    public static final double SURCHARGE_RATE = 1.5;
 
     public static double tick(double distance) {
         if (distance <= INITIAL_CHARGE_DISTANCE) {
             return INITIAL_CHARGE;
         }
-        if (distance <= 8) {
-            return INITIAL_CHARGE + 0.8 * (distance - INITIAL_CHARGE_DISTANCE);
+        if (distance <= SURCHARGE_DISTANCE) {
+            return INITIAL_CHARGE + CHARGE_PER_KILOMETER * (distance - INITIAL_CHARGE_DISTANCE);
         }
-        return INITIAL_CHARGE + 0.8 * (8 - INITIAL_CHARGE_DISTANCE) + 0.8 * (distance - 8) * 1.5;
+        return INITIAL_CHARGE + CHARGE_PER_KILOMETER * (SURCHARGE_DISTANCE - INITIAL_CHARGE_DISTANCE) +
+                CHARGE_PER_KILOMETER * (distance - SURCHARGE_DISTANCE) * SURCHARGE_RATE;
     }
 }
