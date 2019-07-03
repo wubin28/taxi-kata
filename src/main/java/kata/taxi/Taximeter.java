aa6026b8d070d05a3203b9f97a5bf32d699511e4
benwu <wubinben@icloud.com>
2019-07-03 16:15:43 +0800

    refactor

--- src/main/java/kata/taxi/Taximeter.java
+++ src/main/java/kata/taxi/Taximeter.java
@@ -1,10 +1,14 @@
 package kata.taxi;
 
 public class Taximeter {
+
+    public static final int INITIAL_CHARGE_DISTANCE = 2;
+    public static final int INITIAL_CHARGE = 6;
+
     public static double tick(double distance) {
-        if (distance <= 2) {
-            return 6;
+        if (distance <= INITIAL_CHARGE_DISTANCE) {
+            return INITIAL_CHARGE;
         }
-        return 0.8 * (distance - 2) + 6;
+        return 0.8 * (distance - INITIAL_CHARGE_DISTANCE) + INITIAL_CHARGE;
     }
 }
